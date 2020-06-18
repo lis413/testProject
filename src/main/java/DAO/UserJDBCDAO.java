4fd94808f52abb27ebf825770209e62c7050b539
lis154 <lis154@mail.ru>
2020-06-18 09:48:36 +0300

    commmit3

--- src/main/java/DAO/UserJDBCDAO.java
+++ src/main/java/DAO/UserJDBCDAO.java
@@ -2,10 +2,7 @@ package DAO;
 
 import model.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,13 +35,35 @@ public class UserJDBCDAO {
         }
     }
 
-    public void changeUser (User user){
-        
+    public void changeUser (User user, String name){
+        String update = "UPDATE users SET name = ? age = ?  money = ? where name = ?";
+        try (PreparedStatement statement = connection.prepareStatement(update)){
+            statement.setString (1,user.getName());
+            statement.setInt (2,user.getAge());
+            statement.setInt(3, user.getMoney());
+            statement.setString(4, name);
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUser(){
         List<User> list = new ArrayList<>();
-        return list;
+        String query = "select * from users";
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (!resultSet.isLast()){
+                resultSet.next();
+                User user = new User(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getInt("age"), resultSet.getInt("money"));
+                list.add(user);
+            }
+            return list;
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public void createTable() throws SQLException {
